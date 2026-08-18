9e0a3b6d10086d866a88933dc0464855e74c2665
NoorAlbusaidi <nooralhuda.albusaidi@gmail.com>
2026-08-18 15:21:37 +0400

    Operators, Comparisons and Control	Flow

--- JavaScript/warmups.js
+++ JavaScript/warmups.js
@@ -5,3 +5,44 @@ let sum = 5;
 let sumTypeBefore = typeof sum; // "number"
 let sum = "five";
 let sumTypeAfter = typeof sum; // "string"
+
+const equality = 10 == "10"; // true, because the string is converted to a number
+const strictEquality = 10 === "10"; // false, because the types are different
+
+const username = "";
+if (username) {
+    console.log("Username is truthy");
+}
+else {
+    console.log("Username is falsy");
+}
+
+const grades = [85, 92, 58, 73, 40];
+for (const grade of grades) {
+    if (grade < 60) {
+        console.log(`Grade ${grade} is failing`);
+    }
+    else {
+        console.log(`Grade ${grade} is passing`);
+    }
+}
+
+const score = 85;
+let grade;
+
+switch (true) {
+
+    case score >= 90:
+        grade = "A";
+        break;
+    case score >= 80:
+        grade = "B";
+        break;
+    case score >= 70:
+        grade = "C";
+        break;
+    default:
+        grade = "F";
+}
+console.log(grade); // Output: B
+
