8ad9ae108779844effa27c522c9f7ddede70c66e
NoorAlbusaidi <nooralhuda.albusaidi@gmail.com>
2026-08-18 15:30:58 +0400

    Function Declaration

--- JavaScript/warmups.js
+++ JavaScript/warmups.js
@@ -46,3 +46,12 @@ switch (true) {
 }
 console.log(grade); // Output: B
 
+// 1. Function Declaration
+function isEvenDeclaration(number) {
+  if (number % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
