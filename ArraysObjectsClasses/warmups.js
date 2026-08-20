04fdc214bcddaf6f5695fe72488499ae2399fcda
NoorAlbusaidi <nooralhuda.albusaidi@gmail.com>
2026-08-20 14:04:31 +0400

    Using push, pop, shift and unshift

--- ArraysObjectsClasses/warmups.js
+++ ArraysObjectsClasses/warmups.js
@@ -1,1 +1,10 @@
 const cities = ["Muscat","Dubai","London","Tokyo","Paris","New York"];
+cities.push("Berlin");
+console.log(cities);
+cities.unshift("Madrid");
+console.log(cities);
+cities.pop();
+console.log(cities);
+cities.shift();
+console.log(cities);
+
